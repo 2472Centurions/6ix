1f1dd66d3f008c7918b83dc0aa695058cf2588f4
2472Centurions <2472centurions@gmail.com>
2017-01-12 16:38:59 -0600

    intial commit

---
+++ src/Actions/goooooooForward.java
@@ -0,0 +1,19 @@
+package Actions;
+
+import Objects.Action;
+
+public class goooooooForward extends Action {
+	public goooooooForward(){
+		
+	}
+	public void startAction(){
+		
+	}
+	public void endAction(){
+		
+	}
+	public void periodic(){
+		
+	}
+	
+}
---
+++ src/Subsystem/drive.java
@@ -0,0 +1,12 @@
+package Subsystem;
+
+import com.ctre.CANTalon;
+
+public class drive {
+	CANTalon motor1,motor2;
+	double speed;
+	public drive(double speed){
+		
+		
+	}
+}
